15b93c83e5fc8032d1e59d73ded45efc5868eb43
rashmi123456984 <rashmi.radika1998@gmail.com>
2021-10-08 07:31:47 -0700

    commiting missing changes in information.py

--- information.py
+++ information.py
@@ -1,4 +1,6 @@
-print ("HAII everyone how can I help you")
-name = input ("enter your name")
-amount =  float(input("enter your amount"))
-print (name,age)
+from person import person
+person=person("abc")
+
+
+
+print(person.value)
